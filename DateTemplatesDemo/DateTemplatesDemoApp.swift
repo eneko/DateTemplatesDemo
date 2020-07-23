9c32844ad4e7f563474c08257ce9228a20fb2f38
Eneko Alonso <eneko.alonso@gmail.com>
2020-07-23 07:31:12 -0700

    Initial Commit

---
+++ DateTemplatesDemo/DateTemplatesDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateTemplatesDemoApp.swift
+//  DateTemplatesDemo
+//
+//  Created by Eneko Alonso on 7/23/20.
+//
+
+import SwiftUI
+
+@main
+struct DateTemplatesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
